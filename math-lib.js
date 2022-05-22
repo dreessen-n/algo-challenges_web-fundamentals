e25eefab7bf1fe0efc2d8452c08024690316209e
dreessen-n <dreessen.edu@gmail.com>
2022-05-22 17:44:55 -0600

    Finish first section: Math lib

--- math-lib.js
+++ math-lib.js
@@ -1,2 +1,20 @@
 // Math library practice
 
+// math.floor() - rounds DOWN to nearest int
+
+var floor = Math.floor(1.8);
+
+// math.ceil() - rounds UP to nearest int
+
+var ceiling = Math.ceil(Math.PI);
+
+// math.random() - generates a random num between 0 (inclusive)
+// and 1 (exclusive)
+
+var random = Math.random();
+
+console.log(floor);
+console.log(ceiling);
+console.log(random);
+
+
