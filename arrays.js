a225470be5505c9abe8b281cd5312e9513da00b9
dreessen-n <dreessen.edu@gmail.com>
2022-05-22 16:33:35 -0600

    Finish for loops, move on to reverse and array

--- arrays.js
+++ arrays.js
@@ -7,5 +7,29 @@ fruit2 = fruit1;
 
 console.log(`${fruit2} and ${fruit1}`);
 
+// next version
+
+var fruit1 = 'apples';
+var fruit2 = 'oranges';
+
+var temp = fruit1;
+fruit1 = fruit2;
+fruit2 = temp;
+
+console.log(`${fruit2} and ${fruit1}`);
+
+// while loops
+
+var start = 0;
+var end = 12;
+
+while (start < end) {
+	console.log(`start: ${start}, end: ${end}`);
+	start += 2;
+	end -= 2;
+}
+
+
+
 
 
